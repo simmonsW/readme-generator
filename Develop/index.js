ec0a20eeffd20591be27eb11af88d8a03c5ffe28
Wyatt Simmons <simmonsw182@gmail.com>
2021-04-26 16:44:18 -0700

    initial prompt and first question

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'What is the name of your project',
+    validate: projectName => {
+      if (projectName) return true;
+      else {
+        console.log('Please enter the name of your project');
+        return false;
+      };
+    }
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+  inquirer.prompt(questions)
+};
 
 // Function call to initialize app
 init();
