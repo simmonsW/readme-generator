08a6a7e5c44cecc9eb6e2d5a623702233c62f514
Wyatt Simmons <simmonsw182@gmail.com>
2021-04-26 17:19:26 -0700

    added all questions for prompt

--- Develop/index.js
+++ Develop/index.js
@@ -16,6 +16,113 @@ const questions = [
       };
     }
   },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe your project',
+    validate: description => {
+      if (description) return true;
+      else {
+        console.log('Please enter a description for your project');
+        return false;
+      };
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'tableOfContents',
+    message: 'Would you like to include a table of contents for your README?',
+    default: false
+  },
+  {
+    type: 'input',
+    name: 'install',
+    message: 'How do you install your project?',
+    validate: install => {
+      if (install) return true;
+      else {
+        console.log('Please enter the install process for your project');
+        return false;
+      };
+    }
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use your project?',
+    validate: usage => {
+      if (usage) return true;
+      else {
+        console.log('Please enter how to use your project');
+      };
+    }
+  },
+  {
+    type: 'input',
+    name: 'collab',
+    message: "List your project's collaborators",
+    validate: collab => {
+      if (collab) return true;
+      else {
+        console.log('Please enter your collaborators for your project');
+        return false;
+      };
+    }
+  },
+  {
+    type: 'input',
+    name: 'contribute',
+    message: 'how can others contribute to your project?',
+    validate: contribute => {
+      if (contribute) return true;
+      else {
+        console.log('Please enter how others can contribute to your project');
+        return false;
+      };
+    }
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'How can others test your project?',
+    validate: test => {
+      if (test) return true;
+      else {
+        console.log('Please enter how others can test to your project');
+        return false;
+      };
+    }
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'Choose one of the following licenses for your project:',
+    choices: ['MIT', 'GNU', 'Other', 'None']
+  },
+  {
+    type: 'insert',
+    name: 'github',
+    message: 'Enter your GitHub username',
+    validate: github => {
+      if (github) return true;
+      else {
+        console.log('Please enter your GitHub username');
+        return false;
+      };
+    }
+  },
+  {
+    type: 'insert',
+    name: 'email',
+    message: 'Enter your email address',
+    validate: email => {
+      if (email) return true;
+      else {
+        console.log('Please enter your email address');
+        return false;
+      };
+    }
+  }
 ];
 
 // TODO: Create a function to write README file
