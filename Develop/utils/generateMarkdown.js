2459408535b81206d5b1fd610d8f7a83bb0d3789
Wyatt Simmons <simmonsw182@gmail.com>
2021-04-29 16:50:56 -0700

    added createTableOfContents function and mock README

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,11 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return license.map(element => {
-    console.log(element)
-    if (element === 'MIT') {
+  return license.map(el => {
+    console.log(el)
+    if (el === 'MIT') {
       return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/Licenses/MIT)`;
-    } else if (element === 'GNU') {
+    } else if (el === 'GNU') {
       return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
     } else {
       return;
@@ -15,7 +15,9 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
+
+// Included in the badge ^^^^
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -62,13 +64,30 @@ function renderLicenseSection(license) {
   });
 };
 
+const createTableOfContents = flag => {
+  if (flag) {
+    return `
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [How to Use](#How-to-use)
+  * [Collaborators](#collaborators)
+  * [Contribute](#contribute)
+  * [Test](#test)
+  * [Questions](#questions)
+  * [License](#license)
+    `
+  }
+  return '';
+};
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
 
   ${renderLicenseBadge(data.license)}
   # ${data.projectName}
-
+  ${createTableOfContents(data.tableOfContents)}
   ## Description
   ${data.description}
 
